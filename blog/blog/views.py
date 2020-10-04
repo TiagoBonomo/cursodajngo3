449aa371dfad613d44a008b7de597df82f43e21b
TiagoBonomo <tiagofbonomo@gmail.com>
2020-10-03 21:52:23 -0300

    first commit

---
+++ blog/blog/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+
+def hello_world(request):
+
+    return HttpResponse(" ----- Hello World !!!! ------")
+
+
+def teste0001(request):
+
+    return HttpResponse(" ----- teste0001 !!!! ------")
+
+
+def teste0002(request):
+
+    return HttpResponse(" ----- teste0002 !!!! ------")
