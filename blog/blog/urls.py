ce71a8e59c1d9811de81b127009dd9a682191d70
TiagoBonomo <tiagofbonomo@gmail.com>
2020-10-03 22:39:59 -0300

    commit 031020202239

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,15 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import hello_world
-from .views import teste0001
-from .views import teste0002
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',hello_world),
-    path('teste0001/',teste0001),
-    path('teste0002/',teste0002),
+    path('blog/',include('website.urls')),
+    
 
 ]
