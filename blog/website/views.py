00b92cec2e693d8b8546f94570fd869318621970
TiagoBonomo <tiagofbonomo@gmail.com>
2020-10-04 12:52:44 -0300

    13.aula-12

--- blog/website/views.py
+++ blog/website/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_blog(request):
-    return render(request,'index.html')
+
+    #lista de Tecnologias
+    lista = [
+        'Django','Python','Git','Html','Banco de Dados',
+        'Linux','Nginx','Uwsgi','Systemctl'
+    ]
+    #Dicionário de dados
+    data = {
+        'name':'Curso de Django 3',
+        'lista_tecnologias': lista
+    }
+    return render(request,'index.html', data)
