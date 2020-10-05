fe7dbd59c7ec7681d486f99645f5b4a2906d3695
TiagoBonomo <tiagofbonomo@gmail.com>
2020-10-04 21:19:36 -0300

    20.aula 19

--- blog/website/models.py
+++ blog/website/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Categorias(models.TextChoices):
+    TECH = 'TC','Tecnologia'
+    CR = 'CR','Curiosidades'
+    GR = 'GR','Geral' 
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
+
+    categories = models.CharField(
+        max_length=3,
+        choices=Categorias.choices,
+        default=Categorias.GR
+    )
+
+    deleted = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_category_label(self):
+        return self.get_categories_display()
+    
--- blog/website/views.py
+++ blog/website/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def hello_blog(request):
+
+    #Lista de posts vinda do Banco de Dados
+    list_posts = Post.objects.filter(deleted=False)
 
     #lista de Tecnologias
     lista = [
         'Django','Python','Git','Html','Banco de Dados',
         'Linux','Nginx','Uwsgi','Systemctl'
     ]
+
     #Dicionário de dados
     data = {
         'name':'Curso de Django 3',
-        'lista_tecnologias': lista
+        'lista_tecnologias': lista,
+        'posts':list_posts,
     }
+    
     return render(request,'index.html', data)
